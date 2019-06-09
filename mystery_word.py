c3e055bfc7cd60b9c3706eed55248daac892bb1b
Meagan-M <meaganmakinson80@gmail.com>
2019-06-09 19:23:55 -0400

    now can choose mode (easy, hard, regular) and will ask user to enter again if input is  not e,r,h

--- mystery_word.py
+++ mystery_word.py
@@ -1,18 +1,41 @@
 
-word = "sun"
+word = "bubble"
+word_length = len(word)
 tries = 8
 user_attempts = 1
 current_guesses = []
 incorrect_letters = []
 
+print("\n\nWelcome to the Mystery Word Game!\n\nYou have 8 tries to guess the letters of the word")
+while True:
+    game_mode = input("Choose (E)ASY, (R)EGULAR, or (H)ARD mode to begin: ")
+    if game_mode.lower() not in ('e', 'r', 'h'):
+        print("Please choose again\n'e' for easy mode\n'r' for regular mode\n'h' for hard mode\n\n")
+    else:
+        break
+
+
+if game_mode.lower() == "e":
+    print("You have chosen easy mode")
+if game_mode.lower() == "r":
+    print("You have chosen regular mode")
+if game_mode.lower() == "h": 
+    print("You have chosen hard mode")
+
+# This is from the lists and comprehensions example 49
 def print_word(word, guesses):
-    output_letters = [display_letter(letter, guesses) 
+    ''' 
+    this function is from example 49 and is needed along with the code from 48 in order to display the letters and guesses. I tried it without and it didn't work
+    '''
+    output_letters = [display_letter(letter, guesses)
                       for letter in word]
     print(" ".join(output_letters))
 
+# this is from example 48 on lists and comprehensions. I still don't fully understand why both are needed in order for it to work-- meeting with Andy in the morning and will be sure to ask!
+
 def display_letter(letter, guesses):
     """
-    Conditionally display a letter. If the letter is already in
+    example 48: Conditionally display a letter. If the letter is already in
     the list `guesses`, then return it. Otherwise, return "_".
     """
     if letter in guesses:
@@ -22,13 +45,15 @@ def display_letter(letter, guesses):
 
 [display_letter(letter, current_guesses)
  for letter in word]
-    
+
+print("The word has " + str(word_length) + " letters.")
+
 while user_attempts <= tries:
     letter_guess = input("Guess one letter: ")
     print(letter_guess)
     user_attempts += 1
     if letter_guess in word:
-        print(letter_guess + " is in the word! Guess another letter. ") 
+        print(letter_guess + " is in the word! Guess another letter. ")
         current_guesses.append(letter_guess)
         # print(current_guesses)
     else:
@@ -36,16 +61,3 @@ while user_attempts <= tries:
         incorrect_letters.append(letter_guess)
         print(incorrect_letters)
     print_word(word, current_guesses)
-
-
-
-
-
-
-
-
-
-
-
-
-
